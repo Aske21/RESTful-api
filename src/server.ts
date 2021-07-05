c4478d0dbdc2c019e1b5c2bf0643a475aa6dfd87
Aske21 <veledarevica@yahoo.com>
2021-07-05 22:03:14 +0200

    working

---
+++ src/server.ts
@@ -0,0 +1,70 @@
+import http from "http";
+import express from "express";
+import bodyParser from "body-parser";
+
+import logging from "./config/logging";
+import config from "./config/config";
+
+const NAMESPACE = "Server";
+const router = express();
+
+// logging
+
+router.use((req, res, next) => {
+  // log the req
+  logging.info(
+    NAMESPACE,
+    `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+  );
+
+  res.on("finish", () => {
+    // log res
+    logging.info(
+      NAMESPACE,
+      `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`
+    );
+  });
+
+  next();
+});
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+// rules of api
+// in prod ips and routes should be predefined //
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
+// routes
+
+// err handling
+router.use((req, res, next) => {
+  const error = new Error("not found");
+
+  return res.status(404).json({
+    message: error.message,
+  });
+});
+
+// server creating
+
+const httpServer = http.createServer(router);
+httpServer.listen(config.server.port, () =>
+  logging.info(
+    NAMESPACE,
+    `Server is running ${config.server.hostname}:${config.server.port}`
+  )
+);
